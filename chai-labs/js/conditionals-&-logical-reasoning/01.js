d4bc07400f8558f760ac5eb6c4d2eb2c2bc6cec4
thevishaal <thevishaal0@gmail.com>
2026-07-10 20:52:23 +0530

    chai-labs: conditonals: 01.js

---
+++ chai-labs/js/conditionals-&-logical-reasoning/01.js
@@ -0,0 +1,15 @@
+/**
+ * Instructions:- Create a variable named `hasAccess` and
+ * set it to `true`. Then write an if statement that
+ * checks if `hasAccess` is true. If it is, log the message
+ * 'Access granted' to the console.
+ */
+
+// solution
+
+// Create the hasAccess variable
+let hasAccess = true;
+// Write your if statement below
+if (hasAccess) {
+  console.log("Access granted");
+}
