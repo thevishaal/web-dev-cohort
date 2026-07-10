3551025e7fc8fe7678f237fff8f427642d47ad26
thevishaal <thevishaal0@gmail.com>
2026-07-10 20:56:25 +0530

    chai-labs: conditionals: 02.js

---
+++ chai-labs/js/conditionals-&-logical-reasoning/02.js
@@ -0,0 +1,14 @@
+/**
+ * Instructions:- Create a variable named `passwordCorrect` and set it to `false`. Write an if-else statement that logs 'Welcome to the vault' if the password is correct, or 'Access denied' if it's not.
+ */
+
+// solution
+
+// Create the passwordCorrect variable
+const passwordCorrect = false;
+// Write your if-else statement below
+if (passwordCorrect) {
+  console.log("Welcome to the vault");
+} else {
+  console.log("Access denied");
+}
