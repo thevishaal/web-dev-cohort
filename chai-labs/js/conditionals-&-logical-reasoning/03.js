28cafff2f63985275bc78b64b44e0bdae17ad26d
thevishaal <thevishaal0@gmail.com>
2026-07-16 20:45:18 +0530

    chailabs: conditionals:  03.js

---
+++ chai-labs/js/conditionals-&-logical-reasoning/03.js
@@ -0,0 +1,16 @@
+/**
+ * Create a variable named `clearanceLevel` and set it to `2`. Write an if-else-if-else chain that logs: - 'Maximum access granted' if level is 3 - 'Standard access granted' if level is 2 - 'Limited access granted' if level is 1 - 'No access' for any other value
+ */
+
+// solution
+
+// Create the clearanceLevel variable
+let clearanceLevel = 2
+// Write your if-else-if-else chain below
+if(clearanceLevel === 3){
+  console.log("Maximum access granted")
+}else if(clearanceLevel === 2){
+  console.log("Standard access granted")
+}else{
+  console.log("No access")
+}
