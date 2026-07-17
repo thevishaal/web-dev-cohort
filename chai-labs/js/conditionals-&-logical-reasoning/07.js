60dbe8070b5c79193cf2c471d70cc6498d5af044
thevishaal <thevishaal0@gmail.com>
2026-07-17 16:03:05 +0530

    chailabs: conditionals: 07.js  & 08.js

---
+++ chai-labs/js/conditionals-&-logical-reasoning/07.js
@@ -0,0 +1,12 @@
+/**
+ * Create a variable named `temperature` and set it to `25`. Use a ternary operator to create a variable named `status` that equals 'Hot' if temperature is greater than 30, otherwise 'Cool'. Log the status to the console.
+ */
+
+// solution
+
+// Create the temperature variable
+const temperature = 25;
+// Use ternary operator to set status
+const status = temperature > 30 ? "Hot" : "Cool";
+// Log the status
+console.log(status);
