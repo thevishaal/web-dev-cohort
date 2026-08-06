7ceffb8dff4150a165747008d315c1f5f9746ad7
thevishaal <thevishaal0@gmail.com>
2026-08-06 13:22:35 +0530

    http server create

--- practice/backend/repeat-auth-with-ts/src/index.ts
+++ practice/backend/repeat-auth-with-ts/src/index.ts
@@ -1,1 +1,19 @@
-console.log("hello i am from ts something ");
+import http from "node:http";
+import { env } from "./env.js";
+
+async function main() {
+  try {
+    const server = http.createServer();
+
+    const PORT: number = env.PORT ? +env.PORT : 8080;
+
+    server.listen(PORT, () => {
+      console.log(`Server is running on PORT: ${PORT}`);
+    });
+  } catch (error) {
+    console.log("Error coming from starting...");
+    throw error;
+  }
+}
+
+main();
